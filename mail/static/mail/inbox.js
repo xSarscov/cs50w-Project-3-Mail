4846e85da566d9175616fe843c5a04e63f335f95
xSarscov <vargasernest7@gmail.com>
2024-02-13 17:56:35 -0600

    Delete cancel button :bat:

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -284,26 +284,6 @@ function showEmail(id, archiveText, mailbox) {
         document.querySelector('#compose-recipients').value = mailbox === 'sent' ? email.recipients : email.sender;
         document.querySelector('#compose-subject').value = email.subject.includes('Re: ') ? email.subject : 'Re: ' + email.subject;
         document.querySelector('#compose-body').value = `\n\n\n <b> On ${email.timestamp} ${email.sender} wrote:</b> ${email.body}`;
-
-        const cancelButton = document.createElement('button');
-        cancelButton.type = 'button';
-        cancelButton.classList.add('btn', 'btn-outline-danger');
-        cancelButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-        </svg>
-        Cancel
-        `;
-
-        cancelButton.addEventListener('click', () => {
-          showEmail(id, archiveText, mailbox);
-          document.querySelector('#compose-view').style.display = 'none';
-          cancelButton.remove();
-        });
-
-        document.querySelector('#compose-form').appendChild(cancelButton);
-
       });
 
       document.querySelector('#emails-view').style.display = 'none';
